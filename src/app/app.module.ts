9c96e0db9af0f2ce1b029eb8ec362dd2152d99c3
Mihail <60568191+MixailLOL@users.noreply.github.com>
2025-03-10 10:57:46 +0300

    edit btn redirect to edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { EditCountryComponent } from './common-ui/edit-country/edit-country.comp
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { EditPageComponent } from './pages/edit-page/edit-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    RouterLink,
   ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent]
